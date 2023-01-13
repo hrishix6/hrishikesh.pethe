547cbcfad6a167822678b38087a9455998750c14
Hrishikesh Pethe <hrishikesh.pethe@tietoevry.com>
2023-01-13 15:00:09 +0530

    add avatar component

---
+++ components/avatar.tsx
@@ -0,0 +1,11 @@
+export default function Avatar() {
+  return (
+    <div className="w-36 h-36 mx-auto">
+      <img
+        src="avatar.webp"
+        alt="my avatar"
+        className="shadow rounded-full max-w-full h-auto align-middle border-none"
+      />
+    </div>
+  );
+}
